f119e4c0fe41c5ef21159f72fd354e418fde555f
Nieva, Juan Pablo <juampi.np@hotmail.com>
2021-02-13 01:25:50 -0300

    Etapa Inicial

---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    public class ProviderService : BaseService<Provider>
+    {
+        public ProviderService(IRepository<Provider> repository) : base(repository)
+        {
+
+        }
+
+        public new Provider Create(Provider entity)
+        {
+            return base.Create(entity);
+        }
+
+        public Provider Update(Provider entity)
+        {
+            return base.Update(entity);
+        }
+
+        public void Delete(Provider entity) => base.Delete(entity);
+
+        public IEnumerable<Provider> Search(Expression<Func<Provider, bool>> filter)
+        {
+            return this.Repository.List(filter);
+        }
+
+    }
+}
